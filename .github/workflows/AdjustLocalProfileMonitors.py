c14fb86dd94940e66faa059d979a3ab89797a887
BlueFinBima <BlueFinBima@users.noreply.github.com>
2023-12-28 10:50:30 +0000

    Workflow changes
    #none

---
+++ .github/workflows/AdjustLocalProfileMonitors.py
@@ -0,0 +1,55 @@
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Script to make adjustments to the Helios profile by adding in
+# additional monitors by XML manipulation.  This should be more
+# reliable than using diff-match-patch.
+# There are still changes to be made after this script, but it is more
+# efficient for these to be text substitutions.  These changes are not 
+# committed.
+# Arguments:
+# 1. Filename for the original Helios Profile file
+# 2. Filename for the monitor XML to be inserted into the profile 
+# 3. Filename of the resultant Helios profile
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+import sys
+InputHeliosProfile = sys.argv[1]
+hpfMonitorFileName1 = sys.argv[2]
+OutputHeliosProfile = sys.argv[3]
+
+print("Donor Helios Profile", InputHeliosProfile)
+print("Monitor XML ", hpfMonitorFileName1)
+print("New Helios Profile ", OutputHeliosProfile)
+
+from defusedxml.ElementTree import parse
+# parse the original input file
+print("Reading existing profile: ",InputHeliosProfile)
+et = parse(InputHeliosProfile)
+root = et.getroot()
+print("Reading additional monitor XML: ",hpfMonitorFileName1)
+monitorsRoot = parse(hpfMonitorFileName1).getroot()
+
+monitors = root.find('Monitors')
+i = 0
+for el in monitors.iter("Monitors"):
+    i += 1
+    print("Processing Monitor", i, " size ", el.Find("Size").text)
+
+
+## Alter the location of the monitor so that it is positioned at the far right
+#for el in root.iter("Monitor"):
+#    if el.find("Size").text != "1920x1080":
+#       el.
+#    el.find("Location").text = "3840,0"
+#
+## Insert the two additional monitors at the start of the monitor list 
+#for el in root:
+#    if el.tag == "Monitors":
+#        i = 0
+#        for mel in monitorsRoot:
+#            el.insert(i,mel)
+#            i += 1
+#            print("Removing Monitor",i," at ", mel.find("Location").text)
+#        continue
+
+print("Writing new profile: ",OutputHeliosProfile)
+et.write(OutputHeliosProfile,encoding="UTF-8",xml_declaration=True)
+
